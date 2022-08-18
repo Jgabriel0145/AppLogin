151a45144e190b9bcc59d0ca86238ec3a92dead9
Usuario <Usuario@CPD02-M07>
2022-08-18 14:38:50 -0300

    Adicionar arquivos de projeto.

---
+++ AppLogin/AppLogin/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppLogin.Model;
+using AppLogin.View;
+
+namespace AppLogin
+{
+    public partial class App : Application
+    {
+
+        public List<DadosUsuario> list_usuarios = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Email = "aluno@etec",
+                Senha = "123",
+                Nome = "Joao"
+            },
+
+            new DadosUsuario()
+            {
+                Email = "prof@etec",
+                Senha = "456",
+                Nome = "Prof"
+            }
+        };
+
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new Protegida();
+            else
+                MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppLogin/AppLogin/Model/DadosUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppLogin.Model
+{
+    public class DadosUsuario
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+    }
+}
